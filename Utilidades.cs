c17e0bdf21fd0defe3005e398f918fbb2b99ad90
edu1107zera <128429561+edu1107zera@users.noreply.github.com>
2023-07-20 16:15:24 -0300

    Add files via upload

---
+++ Utilidades.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula08Estoque
+{
+    internal class Utilidades
+    {
+        public void mostraMensagem()
+        {
+            MessageBox.Show("ola:)");
+        }
+
+        public bool txtBoxEstaVazio(TextBox txt)
+        {
+            if (txt.Text == "")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+}
+    
+
